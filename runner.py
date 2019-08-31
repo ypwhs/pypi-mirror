f247779487edf831915b42777cdad24eed7eca82
杨培文 (Yang Peiwen) <915505626@qq.com>
2019-08-31 20:32:47 +0800

    Update runner.py

--- runner.py
+++ runner.py
@@ -4,13 +4,22 @@ import time
 from subprocess import run
 
 
+def get_env(key, default=None):
+    value = os.environ.get(key, default)
+    print(f'{key} = {value}')
+    return value
+
+
 def main() -> None:
-    interval = os.environ.get('INTERVAL', 3600)
+    interval = get_env('INTERVAL', 3600)
+    config_path = get_env('CONFIG', '/data/bandersnatch.conf')
+    extra_args = get_env('EXTRA', 'mirror --force-check')
 
     print(f"Running bandersnatch every {interval}s", file=sys.stderr)
     while True:
         start_time = time.time()
-        run(["/usr/bin/bandersnatch", "mirror"])
+        cmd = ['bandersnatch', '-c', config_path, extra_args.split(' ')]
+        run(cmd)
         run_time = time.time() - start_time
         if run_time < interval:
             sleep_time = interval - run_time
